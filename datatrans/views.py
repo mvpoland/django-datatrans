6fcb156dec1b386a962a5aca631f802dacb76b46
lui <lukasz.lesko@mobilevikings.pl>
2020-09-04 13:40:36 +0200

    .

--- datatrans/views.py
+++ datatrans/views.py
@@ -5,7 +5,12 @@ from django.urls import reverse
 from django.conf import settings
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.contenttypes.models import ContentType
-from django.utils.text import gettext_lazy as _
+try:
+    # py2
+    from django.utils.text import ugettext_lazy as _
+except ImportError:
+    # py3
+    from django.utils.text import gettext_lazy as _
 
 from datatrans import utils
 from datatrans.models import KeyValue
